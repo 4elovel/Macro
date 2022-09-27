5b8640c7037e57bc8211df6146259c66207e4e4f
4elovel <marcynakvova007@gmail.com>
2022-09-27 23:05:00 +0300

    Добавьте файлы проекта.

---
+++ Macro/Macro.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Python3.h"
+using namespace std;
+
+int main()
+{
+    seed
+    print("abcd")
+    int mas[10];
+        for (size_t i = 0; i < 10; i++)
+        {
+            mas[i] = random(100)
+            print(mas[i])
+        }
+        print("")
+        for (size_t i = 0; i < 10; i++)
+        {
+            if (mas[i] > 50)
+            {
+                printt(i, " symbol is in (51-100)!")
+            }
+        }
+}
---
+++ Macro/Python3.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#define print(text) cout << text << endl;
+#define input(textt) cin >> textt; 
+#define andd &&
+#define orr ||
+#define elif(abcc){b} else if(abcc){b}
+#define seed srand(time(NULL));
+#define random(x) rand() % x;
+#define printt(i, b) cout << i << b << endl;
